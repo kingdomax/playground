ddf5bc8592a15d6c29bd2cab31bdbec616b154c9
kingdomax <kingdomax@windowslive.com>
2018-05-13 17:09:13 +0700

    add cost of new

---
+++ CostOfNew2/CostOfNew2/SomeObjectWithFields.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostOfNew2
+{
+    public class SomeObjectWithFields
+    {
+        public int IntValue
+        {
+            get;
+            set;
+        }
+
+        public string StringValue
+        {
+            get;
+            set;
+        }
+
+        public DateTime DateValue
+        {
+            get;
+            set;
+        }
+    }
+}
